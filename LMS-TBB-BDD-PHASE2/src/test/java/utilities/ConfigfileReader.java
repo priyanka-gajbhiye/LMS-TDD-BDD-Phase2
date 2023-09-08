0b5ecf914a5de2a41abd12f8871937da1686e80b
priya <priyankagajbhiye110415@gmail.com>
2023-09-08 15:51:47 -0400

    Framework LMS

---
+++ LMS-TBB-BDD-PHASE2/src/test/java/utilities/ConfigfileReader.java
@@ -0,0 +1,47 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigfileReader {
+	
+	private static Properties properties;
+	private final static String propertyFilePath = "./src/test/resources/config/config.properties";
+
+	public static void loadConfig() throws Throwable {
+
+		try {
+			FileInputStream fis;
+			fis = new FileInputStream(propertyFilePath);
+			properties = new Properties();
+			try {
+				properties.load(fis);
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException(" Not found Configuration.properties " + propertyFilePath);
+		}
+	}
+	public static String getBrowserType() {
+		String browser = properties.getProperty("browser");
+		if (browser != null)
+			return browser;
+		else
+			throw new RuntimeException("browser not available. ");
+	}
+
+	public static String getApplicationUrl() {
+		String url = properties.getProperty("url");
+		if (url != null)
+			return url;
+		else
+			throw new RuntimeException("url not available. ");
+	}
+
+
+}
